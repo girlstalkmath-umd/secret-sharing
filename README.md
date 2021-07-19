29fc58eecb11407f325a6ab4f893dbb209331ccb
nglaeser <nglaeser@users.noreply.github.com>
2021-07-19 03:10:11 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Packet: Cryptographic Secret Sharing
+*[Girls Talk Math Camp 2021](http://gtm.math.umd.edu/virtualcamp2021.html) at UMD*
+
+- **Packet** [[source](main.tex) | [.pdf](main.pdf)]: General outline of the file stolen from [this other GTM packet](https://github.com/Girls-Talk-Math/curriculum/tree/master/RSA-Encryption-Cryptography). 
+- **Answers** [[source](answers.tex) | [.pdf](answers.pdf)]
+- **Jupyter Notebook** [[source](jupyter/ShamirSS.ipynb) | [CoLab](https://colab.research.google.com/drive/18NxRFaAb3H65EaUUPwlHN7pQFXYvsy6n?usp=sharing)]: Jupyter notebook for interactive portion.
---
+++ jupyter/helper/helper.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+from numpy import linspace
+from random import randint
+from sympy import isprime, nextprime
+from matplotlib.pyplot import plot, scatter
+from scipy.interpolate import lagrange
+
+######## polynomial stuff ########
+def term_to_string(coeff, deg):
+    if coeff == 0:
+        return ""
+
+    temp = "{}".format(coeff)
+    app = ""
+    # constant term
+    if deg == 0:
+        return temp
+    # x term
+    elif deg == 1:
+        app = "x"
+    # others
+    else:
+        app = "x^{}".format(deg)
+    return app if coeff == 1 else temp+app
+
+def print_poly(coeffs):
+    poly_str = ""
+    
+    # coefficients from highest to lowest degree
+    deg = len(coeffs)-1
+    for i in range(len(coeffs)):
+        if(coeffs[i]!=0):
+            poly_str += "{} + ".format(term_to_string(coeffs[i], deg-i))
+    
+    # remove extra + at end
+    print(poly_str[:-3])
+    
+def eval_poly(coeffs, x):
+    # coefficients from highest to lowest degree
+    deg = len(coeffs)-1
+    ans = 0
+    for i in range(len(coeffs)):
+        ans += coeffs[i]*x**(deg-i)
+        
+    # in real SSS, this is over a finite field (mod p)
+    # return ans%p
+    
+    # but this is not representable in 2D so we will use int arith
+    # (note this is not secure)
+    return ans
+
+# Lagrange interpolation
+def interpolate(points):
+    f = lagrange([p[0] for p in points], [p[1] for p in points])
+    return f
+
+# Lagrange interpolation at a point
+def interpolate_at(points, x_value):
+    # Lagrange basis polynomials evaluated at 0
+    ell = [1]*len(points)
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if i!=j:
+                ell[i] *= float(x_value-points[j][0])/(points[i][0]-points[j][0])
+
+    # f(X) = sum_1^{t+1} ell_i(X) * y_i
+    # y = f(x_value)
+    y = 0
+    for i in range(len(points)):
+        y += ell[i]*points[i][1]
+    return y
+
+######## convert between message and arr of numbers ########
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+def convert_msg(string):
+    arr = []
+    for c in string.lower():
+        try:
+            i = alphabet.index(c)+1
+            arr.append(i)
+        except:
+            continue
+    return arr
+
+def nums_to_msg(arr):
+    msg = ""
+    for num in arr:
+        try:
+            msg += alphabet[num-1]
+        except Exception as e:
+            print("nums_to_msg: ERROR {} ({} is <1 or >26). Make sure you provided at least t+1 shares to the reconstruction function".format(e, num))
+#             msg += "¿"
+#             continue
+            return
+    return msg
